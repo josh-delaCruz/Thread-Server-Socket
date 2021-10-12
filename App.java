b7bee56f86860a67ee5cb613f3aacad3207fbe34
josh-delaCruz <75943649+josh-delaCruz@users.noreply.github.com>
2021-10-12 19:12:55 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+
+package Server;
+
+public class App {
+
+   
+    public static void main(String[] args) {
+        MultiServer server = new MultiServer();
+        server.start();
+    }
+    
+}
---
+++ MultiServer.java
@@ -0,0 +1,26 @@
+
+package Server;
+
+import java.net.*;
+import java.io.*;
+
+
+public class MultiServer {
+   
+    public void start(){
+        try{
+            ServerSocket serverSocket = new ServerSocket(6789);
+            for(;;){
+                System.out.println("1) Server in attesa...");
+                Socket socket = serverSocket.accept();
+                System.out.println("3 Server socket " + socket);
+                ServerThread serverThread = new ServerThread(socket);
+                serverThread.start();
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'istante del server!");
+            System.exit(1);
+        }
+    }
+}
